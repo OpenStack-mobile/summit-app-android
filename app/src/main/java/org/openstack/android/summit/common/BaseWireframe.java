aae88bb406b3599cda449872e0c9c0c16c9d90d9
credi <claudio@tipit.net>
2016-01-20 10:46:25 -0300

    [claudioredi] - Added base wireframe + #10209

---
+++ app/src/main/java/org/openstack/android/summit/common/BaseWireframe.java
@@ -0,0 +1,17 @@
+package org.openstack.android.summit.common;
+
+/**
+ * Created by Claudio Redi on 1/19/2016.
+ */
+public class BaseWireframe implements IBaseWireframe {
+    protected INavigationParametersStore navigationParametersStore;
+
+    public BaseWireframe(INavigationParametersStore navigationParametersStore) {
+        this.navigationParametersStore = navigationParametersStore;
+    }
+
+    @Override
+    public <T> T getParameter(String key, Class<T> type) {
+        return navigationParametersStore.pop(key, type);
+    }
+}
---
+++ app/src/test/java/org/openstack/android/summit/common/data_access/deserialization/SummitAttendeeDeserializerTests.java
@@ -0,0 +1,12 @@
+package org.openstack.android.summit.common.data_access.deserialization;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by Claudio Redi on 1/19/2016.
+ */
+/*@RunWith(RobolectricTestRunner.class)
+public class SummitAttendeeDeserializerTests {
+    
+}*/
