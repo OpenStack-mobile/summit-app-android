ba26e4f6a77062111b519d1acde164856feeae14
credi <claudio@tipit.net>
2016-03-23 11:46:23 -0300

    [claudioredi] - Removed UXTesting framework

--- app/src/main/java/org/openstack/android/summit/OpenStackSummitApplication.java
+++ app/src/main/java/org/openstack/android/summit/OpenStackSummitApplication.java
@@ -17,7 +17,6 @@ import org.openstack.android.summit.dagger.modules.ApplicationModule;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import io.uxtesting.UXTesting;
 
 /**
  * Created by claudio on 11/3/2015.
@@ -49,8 +48,6 @@ public class OpenStackSummitApplication extends Application {
 
         Parse.initialize(this);
         ParseInstallation.getCurrentInstallation().saveInBackground();
-
-        UXTesting.Init(this, "lUMy9RUlm4cQqQeG_1oc_g");
     }
 
     private void initializeInjector() {
