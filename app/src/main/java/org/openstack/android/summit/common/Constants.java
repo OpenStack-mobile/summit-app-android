628324afa1f4d72361e5e5e0f76f21f426f095ec
Sebastian Marcet <smarcet@gmail.com>
2016-09-08 12:03:00 -0300

    [smarcet] * re added files

---
+++ app/src/main/java/org/openstack/android/summit/common/Constants.java
@@ -0,0 +1,30 @@
+package org.openstack.android.summit.common;
+
+/**
+ * Created by Claudio Redi on 11/17/2015.
+ */
+
+public class Constants {
+    static public final String LOG_TAG                              = "OpenStackSummit";
+    static public final String GENERIC_ERROR_MSG                    = "There was an error performing this operation.";
+    static public final String CURRENT_MEMBER_ID                    = "CurrentMemberId";
+    static public final String LOGGED_IN_EVENT                      = "logged-in-event";
+    static public final String LOGGED_OUT_EVENT                     = "logged-out-event";
+    static public final String PUSH_NOTIFICATION_RECEIVED           = "push-notification-received-event";
+    static public final String PUSH_NOTIFICATION_DELETED            = "push-notification-deleted-event";
+    static public final String PUSH_NOTIFICATION_OPENED             = "push-notification-opened-event";
+    static public final String WIPE_DATE_EVENT                      = "wipe-data-event";
+    static public final String NAVIGATION_PARAMETER_SEARCH_TERM     = "SearchTerm";
+    static public final String NAVIGATION_PARAMETER_EVENT_ID        = "EventId";
+    public static final String NAVIGATION_PARAMETER_LEVEL           = "Level";
+    public static final String NAVIGATION_PARAMETER_TRACK           = "Track";
+    public static final String NAVIGATION_PARAMETER_IS_MY_PROFILE   = "IsMyProfile";
+    public static final String NAVIGATION_PARAMETER_SPEAKER         = "SpeakerId";
+    public static final String NAVIGATION_PARAMETER_NOTIFICATION_ID = "NotificationId";
+    public static final String NAVIGATION_PARAMETER_VENUE           = "VenueId";
+    public static final String NAVIGATION_PARAMETER_ROOM            = "roomId";
+    public static final String BASIC_AUTH_USER                      = "org.openstack.android.summit.common.security.BASIC_AUTH_USER";
+    public static final String BASIC_AUTH_PASS                      = "org.openstack.android.summit.common.security.BASIC_AUTH_PASS";
+    public static final String FLAVOR_DEV                           = "development";
+    public static final String FLAVOR_BETA                          = "beta";
+}
